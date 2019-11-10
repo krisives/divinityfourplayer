44a2f53767f8b5d5e610f51cde7262b1772f0617
Martin Chapman <martin.chapman4@mail.dcu.ie>
2019-11-10 22:00:01 +0000

    Added extract_meta_lsx to pull latest meta.lsx file from steam pak file. Requires DOSEE to be installed

---
+++ extract_meta_lsx.bat
@@ -0,0 +1,4 @@
+"./DOS_export_tool-v1.14.1/divine.exe" -a extract-single-file -s "H:\Games\games\SteamApps\common\Divinity Original Sin Enhanced Edition\Data\Main.pak" -d "%CD%/Dos4Player/Resources/meta.lsx" -f "Mods/Main/meta.lsx" 
+"./DOS_export_tool-v1.14.1/divine.exe" -a extract-single-file -s "H:\Games\games\SteamApps\common\Divinity Original Sin Enhanced Edition\Data\Main.pak" -d "%CD%/Dos4Player/Resources/meta.lsx.txt" -f "Mods/Main/meta.lsx" 
+REM https://github.com/Norbyte/lslib
+pause
