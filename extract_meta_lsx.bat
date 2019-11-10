5d77e8ac3630312072591557f2f87b5b47bfa994
Martin Chapman <martin.chapman4@mail.dcu.ie>
2019-11-10 22:11:57 +0000

    Added powershell command to string replace player count from 2 to 4.

--- extract_meta_lsx.bat
+++ extract_meta_lsx.bat
@@ -1,4 +1,5 @@
 "./DOS_export_tool-v1.14.1/divine.exe" -a extract-single-file -s "H:\Games\games\SteamApps\common\Divinity Original Sin Enhanced Edition\Data\Main.pak" -d "%CD%/Dos4Player/Resources/meta.lsx" -f "Mods/Main/meta.lsx" 
-"./DOS_export_tool-v1.14.1/divine.exe" -a extract-single-file -s "H:\Games\games\SteamApps\common\Divinity Original Sin Enhanced Edition\Data\Main.pak" -d "%CD%/Dos4Player/Resources/meta.lsx.txt" -f "Mods/Main/meta.lsx" 
+powershell -Command "(gc "%CD%/Dos4Player/Resources/meta.lsx") -replace 'NumPlayers\" value=\"2', 'NumPlayers\" value=\"4' | Out-File -encoding ASCII "%CD%/Dos4Player/Resources/meta.lsx"
+powershell -Command "Copy-Item ./Dos4Player/Resources/meta.lsx -Destination ./Dos4Player/Resources/meta.lsx.txt"
 REM https://github.com/Norbyte/lslib
 pause
